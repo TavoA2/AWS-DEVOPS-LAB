58b66d87487e1257e699f88e8428fb6fb25d7906
Dixon Araya <dixon.ar08@gmail.com>
2026-08-28 00:05:38 -0600

    fix: use immutable GitHub OIDC subject IDs

--- bootstrap/github-oidc/main.tf
+++ bootstrap/github-oidc/main.tf
@@ -29,8 +29,8 @@ resource "aws_iam_role" "github_actions" {
 
           StringLike = {
             "token.actions.githubusercontent.com:sub" = [
-              "repo:TavoA2@12345678/AWS-DEVOPS-LAB@87654321:pull_request",
-              "repo:TavoA2@12345678/AWS-DEVOPS-LAB@87654321:ref:refs/heads/main"
+              "repo:TavoA2@115035743/AWS-DEVOPS-LAB@1340893898:pull_request",
+              "repo:TavoA2@115035743/AWS-DEVOPS-LAB@1340893898:ref:refs/heads/main"
             ]
           }
         }
