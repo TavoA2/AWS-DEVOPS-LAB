60f9f1b3243c49c1eaaf4cce7c36c86859aad8f3
Dixon Araya <dixon.ar08@gmail.com>
2026-08-28 16:09:36 -0600

    security: document accepted Trivy findings

--- modules/loadbalancing/main.tf
+++ modules/loadbalancing/main.tf
@@ -112,16 +112,6 @@ resource "aws_lb_listener" "http" {
     target_group_arn = aws_lb_target_group.web.arn
   }
 }
-resource "aws_lb_listener" "http" {
-  load_balancer_arn = aws_lb.main.arn
-  port              = 80
-  protocol          = "HTTP"
-
-  default_action {
-    type             = "forward"
-    target_group_arn = aws_lb_target_group.web.arn
-  }
-}
 
 resource "aws_vpc_security_group_ingress_rule" "web_from_alb" {
   security_group_id            = var.web_security_group_id
