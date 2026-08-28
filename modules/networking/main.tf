be389c9be87e1d8f234e88a8a50f37aee15111fb
Dixon Araya <dixon.ar08@gmail.com>
2026-08-28 13:11:03 -0600

    security: restrict app and database outbound access

--- modules/networking/main.tf
+++ modules/networking/main.tf
@@ -148,7 +148,7 @@ resource "aws_security_group" "web" {
   }
 }
 
-resource "aws_vpc_security_group_ingress_rule" "web_https" {
+/*resource "aws_vpc_security_group_ingress_rule" "web_https" {
   security_group_id = aws_security_group.web.id
 
   cidr_ipv4   = "0.0.0.0/0"
@@ -157,6 +157,17 @@ resource "aws_vpc_security_group_ingress_rule" "web_https" {
   ip_protocol = "tcp"
 
   description = "Allow HTTPS from Internet"
+}NOTA: Se reemplaza por el resource de abajo*/
+
+resource "aws_vpc_security_group_egress_rule" "web_https_outbound" {
+  security_group_id = aws_security_group.web.id
+
+  cidr_ipv4   = "0.0.0.0/0"
+  from_port   = 443
+  to_port     = 443
+  ip_protocol = "tcp"
+
+  description = "Allow HTTPS outbound traffic"
 }
 
 resource "aws_vpc_security_group_egress_rule" "web_all_outbound" {
